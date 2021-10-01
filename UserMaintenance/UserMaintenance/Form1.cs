875ab0975ceeb894481fbd5d54ed0b70a62cb473
Ramóna Gáspár <ramona.gaspar@stud.uni-corvinus.hu>
2021-10-01 19:42:00 +0200

    Form1 kialakítása

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -15,6 +15,9 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
+            lblLastName.Text = Resource.LastName;
+            lblFirstName.Text = Resource.FirstName;
+            btnAdd.Text = Resource.Add;
         }
     }
 }
