a54c774f869bf7716fc463968a1e06551c650f2c
Ramóna Gáspár <ramona.gaspar@stud.uni-corvinus.hu>
2021-10-06 19:34:52 +0200

    Formatting

--- UserMaintenance/Week4/Form1.cs
+++ UserMaintenance/Week4/Form1.cs
@@ -22,10 +22,13 @@ namespace Week4
         Excel.Workbook xlWB;
         Excel.Worksheet xlSheet;
 
+
+
         public Form1()
         {
             InitializeComponent();
             LoadData();
+
         }
 
         private void LoadData()
@@ -45,6 +48,8 @@ namespace Week4
                 
                 xlApp.Visible = true;
                 xlApp.UserControl = true;
+
+
             }
 
             catch (Exception ex)
@@ -73,7 +78,65 @@ namespace Week4
                  "Alapterület (m2)",
                  "Ár (mFt)",
                  "Négyzetméter ár (Ft/m2)"};
+
+            xlSheet.Cells[1, 1] = headers[0];
+            object[,] values = new object[Flats.Count, headers.Length];
+
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                values[counter, 1] = f.Vendor;
+                values[counter, 2] = f.Side;
+                values[counter, 3] = f.District;
+                values[counter, 4] = f.Elevator;
+                values[counter, 5] = f.NumberOfRooms;
+                values[counter, 6] = f.FloorArea;
+                values[counter, 7] = f.Price;
+                values[counter, 8] = "";
+                counter++;
+            }
+
+           
+
+            Excel.Range headerRange = xlSheet.get_Range(GetCell(1, 1), GetCell(1, headers.Length));
+            headerRange.Font.Bold = true;
+            headerRange.VerticalAlignment = Excel.XlVAlign.xlVAlignCenter;
+            headerRange.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            headerRange.EntireColumn.AutoFit();
+            headerRange.RowHeight = 40;
+            headerRange.Interior.Color = Color.LightBlue;
+            headerRange.BorderAround2(Excel.XlLineStyle.xlContinuous, Excel.XlBorderWeight.xlThick);
+
+            xlSheet.get_Range(
+                    GetCell(2, 1),
+                    GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
         }
+
+
+
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+
+
+        }
+
+        
+
+
 
 
     }
