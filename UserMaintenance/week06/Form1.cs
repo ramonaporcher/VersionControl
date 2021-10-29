104a429417d8be755b7582dbbb83ace77017a3c2
Ramóna Gáspár <ramona.gaspar@stud.uni-corvinus.hu>
2021-10-29 11:25:44 +0200

    GetData és XmlDocument

--- UserMaintenance/week06/Form1.cs
+++ UserMaintenance/week06/Form1.cs
@@ -32,8 +32,37 @@ namespace week06
             var result = response.GetExchangeRatesResult;
 
             dataGridView1.DataSource = Rates;
+            GetData();
 
-
+            
         }
+
+        private void GetData()
+        {
+            /*
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                
+                var rate = new RateData2();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+            */
+        }
+
+
     }
 }
