4799c9188d6fe919bee76361042a53cc82a23a0e
Ramóna Gáspár <ramona.gaspar@stud.uni-corvinus.hu>
2021-10-29 10:58:17 +0200

    Exchange requests

--- UserMaintenance/week06/Form1.cs
+++ UserMaintenance/week06/Form1.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace week06
 {
+
     public partial class Form1 : Form
     {
+        
+
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MnbServiceReference.MNBArfolyamServiceSoapClient();
+
+            var request = new MnbServiceReference.GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+
         }
     }
 }
